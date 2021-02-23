3c7e8a56a63d6f6dbe1267a555468fe3e3d1b406
Amin Arriaga <16-10072@usb.ve>
2021-02-23 18:17:39 -0500

    Agregada las verificaciones al insertar una prediccion.

--- Haskinator.hs
+++ Haskinator.hs
@@ -3,7 +3,11 @@ import System.IO
 import Data.Maybe (fromJust, isNothing)
 import Data.Map (fromList, toList) 
 
-{- FUNCIONES PARA VERIFICAR LA CORRECTITUD DE UN ORACULO -}
+{- ###### FUNCIONES PARA VERIFICAR LA CORRECTITUD DE UN ORACULO ####### -}
+-- Obtiene una lista con todas las posibles respuestas a una pregunta
+obtenerRespuestas :: Opciones -> [String]
+obtenerRespuestas x = Prelude.map fst (toList x)
+
 -- Recibe unas opciones y retorna todos los oraculos asociados.
 obtenerOraculos :: Opciones -> [Oraculo]
 obtenerOraculos x = Prelude.map snd (toList x)
@@ -23,19 +27,18 @@ obtenerPredsOraculos :: [Oraculo] -> [String]
 obtenerPredsOraculos [] = []
 obtenerPredsOraculos (x:xs) = (obtenerPreds x) ++ (obtenerPredsOraculos xs)
 
--- Funcion para verificar si todos los elementos de una lista son distintos.
-distintos :: Eq a => [a] -> Bool
-distintos [] = True
-distintos (x:xs) 
-  | elem x xs = False
-  | otherwise = distintos xs
-
--- Funcion para verificar si una lista de oraculos es correcta.
-verificarPreds :: [Oraculo] -> Bool
-verificarPreds x = distintos $ obtenerPredsOraculos x 
+-- Recibe un oraculo y retorna una lista con todas las preguntas que aparecen
+-- a partir de el.
+obtenerPregs :: Oraculo -> [String]
+obtenerPregs (Prediccion x) = []
+obtenerPregs (Pregunta text x) = [text] ++ (obtenerPregs' $ obtenerOraculos x)
+  where
+    obtenerPregs' [] = []
+    obtenerPregs' (x:xs) = (obtenerPregs x) ++ (obtenerPregs' xs)
 
 
-{- FUNCIONES PARA IMPRIMIR CON ESTILO -}
+
+{- ################## FUNCIONES PARA IMPRIMIR CON ESTILO ################## -}
 -- Leemos una linea
 prompt :: String -> IO String
 prompt text = do
@@ -80,7 +83,8 @@ putSuccess text =
     verde; putStr " ***\n\n"; normal
 
 
-{- FUNCIONES DE CREACION -}
+
+{- ######################### FUNCIONES DE CREACION ######################### -}
 crearNuevoOraculo :: IO ()
 crearNuevoOraculo = 
   do pred <- prompt "\nIndique la predicción del Oraculo a ser creado:\n"
@@ -88,61 +92,118 @@ crearNuevoOraculo =
      main' (Just $ crearOraculo pred) 
 
 
-{- FUNCIONES DE PREDICCION -}
--- Obtiene una lista con todas las posibles respuestas a una pregunta
-obtenerRespuestas :: Opciones -> [String]
-obtenerRespuestas x = Prelude.map fst (toList x)
 
+{- ######################### FUNCIONES DE PREDICCION ######################### -}
 -- Imprime las opciones disponibles para una pregunta como una lista
 -- para que sea legible para el usuario.
 printOpciones :: [String] -> IO ()  
-printOpciones [] = putStrLn "* Ninguna de las anteriores"
+printOpciones [] = putStr ""
 printOpciones (x:xs) = 
   do
     putStrLn ("* " ++ x)
     printOpciones xs
 
+-- Dada una pregunta, una nueva opcion, una nueva prediccion y un oraculo
+-- retorna el mismo oraculo pero modificando la pregunta tal que se agrega
+-- la nueva opcion con la nueva prediccion.
 agregarOpcion :: String -> String -> String -> Oraculo -> Oraculo
 agregarOpcion pregunta opcion prediccion  (Prediccion texto) = (Prediccion texto)
 agregarOpcion pregunta opcion prediccion  (Pregunta texto ops)
+  -- Si la pregunta actual es la que buscamos, agregamos la nueva opcion y 
+  -- la nueva prediccion
   | texto == pregunta = ramificar nuevasOpciones nuevosOraculos pregunta
+  -- En caso contrario aplicamos recorsivamente la funcion sobre cada uno
+  -- de los sub-oraculos.
   | otherwise = ramificar (obtenerRespuestas ops) opcionesSigNivel texto
   where 
     nuevasOpciones = obtenerRespuestas ops ++ [opcion]
     nuevosOraculos = obtenerOraculos ops ++ [crearOraculo prediccion]
     opcionesSigNivel = map (agregarOpcion pregunta opcion prediccion) (obtenerOraculos ops)
 
+-- Obtenemos una nueva prediccion del usuario.
+obtenerNuevaPred :: String -> Oraculo -> [String] -> IO()
+obtenerNuevaPred pregunta oraculo opciones = 
+  do
+    -- Obtenemos la opcion correspondiente.
+    opcion <- prompt "Diga la nueva opcion:\n"
+    -- Si dicha opcion ya se encuentra, error.
+    if elem opcion opciones then do
+      putError "Esa opcion ya se encuentra en las anteriores."
+      putStrLn "Actuales opciones:"
+      printOpciones opciones
+      obtenerNuevaPred pregunta oraculo opciones
+    else do
 
+      -- Obtenemos la prediccion.
+      prediccion <- prompt "Diga la prediccion correspondiente a la nueva opcion:\n"
+      -- Si ya existe la prediccion en el oraculo principal, error
+      if elem prediccion $ obtenerPreds oraculo then do
+        putError "Esa prediccion ya se puede alcanzar usando mis oraculos."
+        putStrLn "Actuales predicciones:"
+        printOpciones $ obtenerPreds oraculo
+        obtenerNuevaPred pregunta oraculo opciones
+      else do
+
+        -- Actualizamos el oraculo.
+        putSuccess "Oraculo actualizado correctamente!"
+        let oraculo' = agregarOpcion pregunta opcion prediccion oraculo
+        main' $ Just oraculo'
+    
 -- Ejecuta el proceso de predicción hasta que llega a una de las hojas del
 -- arbol o hasta que el usuario contesta ninguna de las anteriores.
 mainPrediccion :: Oraculo -> Oraculo -> IO ()
+-- Si llegamos a una hoja, damos la prediccion
 mainPrediccion (Prediccion texto) mainOraculo = 
   do 
     putStrLn ("Usted está pensando en: " ++ texto ++ "\n")
     respuesta <- prompt "¿Es esto correcto? (Si o No)\n"
+    putStr "\n"
     if respuesta == "Si"
-      then
+      then do
+        -- Si predecimos correctamente, regresamos al main.
         putSuccess "Los humanos son demasiado predecibles..."
-      else
+        main' $ Just mainOraculo
+      else do
         putStrLn "Como que no mmgvo? Estas en drogas?"
+-- Si llegamos a una pregunta, pedimos una opcion al usuario.
 mainPrediccion (Pregunta texto opciones) mainOraculo = 
   do
-    putStrLn (texto ++ "\n")
+    negrita
+    putStrLn (texto)
+    normal
     let opcionesStr = obtenerRespuestas opciones 
     printOpciones opcionesStr 
+    putStrLn "* Ninguna de las anteriores"
     input <- prompt "Ingrese una de las opciones:\n"
+    putStr "\n"
     let oraculo = (Pregunta texto opciones)
-    if elem input opcionesStr 
-      then
-        do
-          mainPrediccion (respuesta oraculo input) mainOraculo
-      else
-        do
-          putError "Por favor, seleccione una opcion correcta."
-          mainPrediccion oraculo mainOraculo
+    -- Verificamos que es una opcion valida y pasamos al siguiente oraculo.
+    if elem input opcionesStr then do
+      mainPrediccion (respuesta oraculo input) mainOraculo
+    -- Si no es ninguna de las opciones anteriores, tenemos que agregar
+    -- una nueva prediccion.
+    else if input == "Ninguna de las anteriores" then do
+      obtenerNuevaPred texto mainOraculo $ obtenerRespuestas opciones
+    -- La opcion no es valida, error
+    else do
+      putError "Por favor, seleccione una opcion correcta."
+      mainPrediccion oraculo mainOraculo
+
+-- Inicia el proceso de prediccion.
+iniciarPrediccion :: Oraculo -> Oraculo -> IO ()
+iniciarPrediccion oraculo mainOraculo =
+  do
+    clear
+    negrita
+    putStrLn "#################################"
+    putStrLn "¡Inicia el proceso de prediccion!"
+    putStrLn "#################################\n"
+    normal
+    mainPrediccion oraculo mainOraculo
 
 
-{- FUNCIONES DE PERSISTENCIA -}
+
+{- ######################## FUNCIONES DE PERSISTENCIA ######################## -}
 persistirOraculo :: Maybe Oraculo -> IO ()
 persistirOraculo oraculo
   | isNothing oraculo = 
@@ -157,7 +218,8 @@ persistirOraculo oraculo
          main' oraculo
 
 
-{- FUNCIONES DE CARGADO -}
+
+{- ########################## FUNCIONES DE CARGADO ########################## -}
 cargarOraculo :: IO ()
 cargarOraculo =
   do fileName <- prompt "\nIndique el nombre del archivo de entrada:\n" 
@@ -170,7 +232,8 @@ cargarOraculo =
      main' (Just oraculo)
 
 
-{- FUNCIONES DE CONSULTA DE PREGUNTA CRUCIAL -}
+
+{- ############### FUNCIONES DE CONSULTA DE PREGUNTA CRUCIAL ############### -}
 -- Ejecuta un recorrido DFS en el oraculo y devuelve una lista con el camino
 -- hasta el nodo buscado.
 dfs :: Oraculo -> String -> Maybe [String]
@@ -198,7 +261,6 @@ obtenerPuntoInflexion (x:xs) (y:ys) r
   | x == y = obtenerPuntoInflexion xs ys x
   | otherwise = Just r
 
-
 consultarPreguntaCrucial :: Maybe Oraculo -> IO ()
 -- Chequear cuando el oraculo es Nothing
 consultarPreguntaCrucial oraculo =
@@ -216,14 +278,30 @@ consultarPreguntaCrucial oraculo =
      main' oraculo
 
 
-{- FUNCIONES DEL MAIN -}
+
+{- ########################## FUNCIONES DEL MAIN ########################## -}
 iniciaFuncion :: Maybe Oraculo -> Char -> IO ()
 iniciaFuncion oraculo op
   | op == '1' = do crearNuevoOraculo
-  | op == '2' = do mainPrediccion (fromJust oraculo) (fromJust oraculo)
-  | op == '3' = do persistirOraculo oraculo
+  | op == '2' = do 
+      if isNothing oraculo then do
+        putError "No se puede iniciar una prediccion sin ningun oraculo cargado."
+        main' oraculo
+      else do 
+        iniciarPrediccion (fromJust oraculo) (fromJust oraculo)
+  | op == '3' = do 
+      if isNothing oraculo then do
+        putError "No se puede guardar un oraculo si no hay ninguno cargado."
+        main' oraculo
+      else do 
+        persistirOraculo oraculo
   | op == '4' = do cargarOraculo
-  | op == '5' = do consultarPreguntaCrucial oraculo
+  | op == '5' = do 
+      if isNothing oraculo then do
+        putError "No se puede consultar una pregunta crucial si no hay ningun oraculo cargado."
+        main' oraculo
+      else do 
+        consultarPreguntaCrucial oraculo
   | op == '6' = do putStrLn (show oraculo)
                    main' oraculo
   | otherwise = do putStrLn "\nHasta la proxima!" 
@@ -279,7 +357,7 @@ oraculosNivel3 = [
 
 oraculosNivel2 = [
   ramificar ["Suicide Squad", "The Dark Knight"] oraculosNivel3 "De que pelicula es el Joker?",
-  crearOraculo "Jared Leto",
+  crearOraculo "Tom Hardy",
   crearOraculo "Cillian Murphy"
   ]
 
